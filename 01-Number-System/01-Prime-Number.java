c6139ced1619a8d0f3c10f460a1608651a8f1f33
aastha07110 <sha.aastha07@gmail.com>
2026-03-31 01:18:02 +0530

    Update 01-Prime-Number.java

--- 01-Number-System/01-Prime-Number.java
+++ 01-Number-System/01-Prime-Number.java
@@ -1,4 +1,25 @@
 // Problem: Check whether a number is prime or not
 // Concept: Loop + Square Root Optimization
 
+public class PrimeNumber{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    
+    boolean isPrime = true;
 
+    
+    if(n <= 1) isPrime = false;
+    else if(n==2) isPrime = true;
+    else if(n % 2 == 0) isPrime = false;
+    else{
+      for(int i = 3; i*i <= n; i=i+2){
+        if(n%i == 0){
+          isPrime = false;
+          break;
+        }
+      }
+    }
+    System.out.println(n + (isPrime? " is Prime ":" is not Prime "));
+  }
+}
