b0a62424550b994722eb0ae4e96c674df70aa771
aastha07110 <sha.aastha07@gmail.com>
2026-04-01 23:44:31 +0530

    Update 03-Palindrome-Number.java

--- 01-Number-System/03-Palindrome-Number.java
+++ 01-Number-System/03-Palindrome-Number.java
@@ -1,1 +1,17 @@
+// Problem: Check whether a number is palindrome or not
+// Concept: Reverse number using loop, modulo (%) and division (/)
 
+
+import java.util.*;
+
+public class PalindromeNumber{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt(), temp = n, rev = 0;
+    while(n>0){
+      rev = rev * 10 + n % 10;
+      n/=10;
+    }
+    System.out.println(temp == rev? " Palindrome ": " Not Palindrome ");
+  }
+}
