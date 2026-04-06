367ec12563df709ba46db810b7558c665015cbee
aastha07110 <sha.aastha07@gmail.com>
2026-04-06 23:00:28 +0530

    Update 09-Sum-of-Digits.java

--- 01-Number-System/09-Sum-of-Digits.java
+++ 01-Number-System/09-Sum-of-Digits.java
@@ -1,13 +1,22 @@
-// Problem:
-// Concept:
-// Time Complexity: 
-// Space Complexity: 
-
+// Problem: Find sum of digits of a number
+// Concept: Use modulo (%) to extract digits and division (/) to remove digits
+// Time Complexity: O(d) ≈ O(log n)
+// Space Complexity: O(1)
 
 import java.util.*;
 
-public class SumOfDigits{
+public class SumOfDigits {
   public static void main(String[] args){
-    
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    int sum = 0;
+
+    while(n > 0){
+      sum += n % 10;  // get last digit
+      n = n / 10;     // remove last digit
+    }
+
+    System.out.println("Sum of digits: " + sum);
   }
 }
