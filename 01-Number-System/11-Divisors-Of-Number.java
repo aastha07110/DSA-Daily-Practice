0b40bcecc58b2dfe96b276ebfe5f1dd95e412fad
aastha07110 <sha.aastha07@gmail.com>
2026-04-08 23:30:27 +0530

    Update 11-Divisors-Of-Number.java

--- 01-Number-System/11-Divisors-Of-Number.java
+++ 01-Number-System/11-Divisors-Of-Number.java
@@ -5,3 +5,22 @@
 
 
 import java.util.*;
+
+public class DivisorsOfNumber{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Number: ");
+    int n = sc.nextInt();
+    System.out.println("Divisors of " + n + ":");
+
+    for(int i = 1; i <= n; i++){
+      if(n%i==0){
+        System.out.println(i + " ");
+        if(i!=n/i){
+          System.out.println((n/i) + " ");
+        }
+      }
+    }
+    sc.close();
+  }
+}
