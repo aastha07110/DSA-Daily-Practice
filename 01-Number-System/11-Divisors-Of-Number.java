c980a9879741ca132da8d9b453fe1b3cb010b40c
aastha07110 <sha.aastha07@gmail.com>
2026-04-08 23:31:26 +0530

    Update 11-Divisors-Of-Number.java

--- 01-Number-System/11-Divisors-Of-Number.java
+++ 01-Number-System/11-Divisors-Of-Number.java
@@ -1,8 +1,7 @@
-// Problem: 
-// Concept: 
-// Time Complexity:
-// Space Complexity: 
-
+// Problem: Find all divisors of a number
+// Concept: Loop till square root and use divisor pairs (i and n/i)
+// Time Complexity: O(√n)
+// Space Complexity: O(1)
 
 import java.util.*;
 
@@ -11,16 +10,19 @@ public class DivisorsOfNumber{
     Scanner sc = new Scanner(System.in);
     System.out.println("Number: ");
     int n = sc.nextInt();
+
     System.out.println("Divisors of " + n + ":");
 
-    for(int i = 1; i <= n; i++){
-      if(n%i==0){
-        System.out.println(i + " ");
-        if(i!=n/i){
-          System.out.println((n/i) + " ");
+    for(int i = 1; i * i <= n; i++){
+      if(n % i == 0){
+        System.out.print(i + " ");
+
+        if(i != n / i){
+          System.out.print(n / i + " ");
         }
       }
     }
+
     sc.close();
   }
 }
