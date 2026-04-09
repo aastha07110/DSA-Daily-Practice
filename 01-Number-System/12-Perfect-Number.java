671cac9a4478054b41baba164e1aad393b296b78
aastha07110 <sha.aastha07@gmail.com>
2026-04-09 23:10:11 +0530

    Create 12-Perfect-Number.java

---
+++ 01-Number-System/12-Perfect-Number.java
@@ -0,0 +1,17 @@
+// Problem:
+// Concept:
+// Time Complexity:
+// Space Complexity:
+
+
+import java.util.*;
+
+public class PerfectNumber{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    int sum = 0;
+  }
+}
