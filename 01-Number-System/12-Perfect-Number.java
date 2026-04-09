e6d5778f8a18ddccc2e838ac55d0f4fc38c80b8b
aastha07110 <sha.aastha07@gmail.com>
2026-04-09 23:12:07 +0530

    Update 12-Perfect-Number.java

--- 01-Number-System/12-Perfect-Number.java
+++ 01-Number-System/12-Perfect-Number.java
@@ -13,5 +13,17 @@ public class PerfectNumber{
     int n = sc.nextInt();
 
     int sum = 0;
+
+    for(int i = 1; i <= n/2; i++){
+      if(n%i == 0){
+        sum = sum + i;
+      }
+    }
+    if(sum == n){
+      System.out.println(n + "is perfect");
+    } else{
+      System.out.println(n + "x");
+    }
+    sc.close();
   }
 }
