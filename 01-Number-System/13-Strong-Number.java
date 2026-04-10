46ed64006dc4786ecd809c9d539d6e40d18e5f7e
aastha07110 <sha.aastha07@gmail.com>
2026-04-10 23:52:34 +0530

    Update 13-Strong-Number.java

--- 01-Number-System/13-Strong-Number.java
+++ 01-Number-System/13-Strong-Number.java
@@ -7,5 +7,14 @@
 import java.util.*;
 
 public class StrongNumber{
-  
+  static int factorial(int n){
+    int fact = 1;
+
+  if(n == 0) return 1;
+
+  for(int i = 1; i<=n; i++){
+    fact = fact*i;
+  }
+  return fact;
+}
 }
