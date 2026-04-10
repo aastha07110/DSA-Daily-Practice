cd116f17acd8ba14a74438e6496d5477f518add7
aastha07110 <sha.aastha07@gmail.com>
2026-04-10 23:57:26 +0530

    Update 13-Strong-Number.java

--- 01-Number-System/13-Strong-Number.java
+++ 01-Number-System/13-Strong-Number.java
@@ -9,12 +9,32 @@ import java.util.*;
 public class StrongNumber{
   static int factorial(int n){
     int fact = 1;
+    if(n==0) return 1;
 
-  if(n == 0) return 1;
-
-  for(int i = 1; i<=n; i++){
-    fact = fact*i;
+    for(int i = 1; i<= n; i++){
+      fact = fact*i;
+    }
+    return fact;
   }
-  return fact;
-}
+
+  static boolean isStrong(int num){
+    int temp = num;
+    int sum = 0;
+
+    while(temp>0){
+      int digit = temp%10;
+      sum = sum + factorial(digit);
+      temp = temp/10;
+    }
+    return sum == num;
+  }
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    if(isStrong(n))
+      System.out.println(n + " strong");
+    else
+      System.out.println()n + " not strong");
+  }
 }
