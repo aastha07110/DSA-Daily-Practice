92cca738ff3a63ed23704cd0626755be4ed18cb6
aastha07110 <sha.aastha07@gmail.com>
2026-04-13 22:55:13 +0530

    Update 14-Count-Digits.java

--- 01-Number-System/14-Count-Digits.java
+++ 01-Number-System/14-Count-Digits.java
@@ -10,7 +10,10 @@ public class CountDigits{
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     System.out.println("Number: ");
-
     
+    int n = sc.nextInt();
+
+    int count = 0;
+    int temp = n;
   }
 }
