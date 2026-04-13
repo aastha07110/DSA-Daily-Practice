2044ccba1969a382c42c591a16a670bdc9981de1
aastha07110 <sha.aastha07@gmail.com>
2026-04-13 22:54:38 +0530

    Update 14-Count-Digits.java

--- 01-Number-System/14-Count-Digits.java
+++ 01-Number-System/14-Count-Digits.java
@@ -1,1 +1,16 @@
+// Problem:
+// Concept: 
+// Time Complexity:
+// Space Complexity:
 
+
+import java.util.*;
+
+public class CountDigits{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Number: ");
+
+    
+  }
+}
