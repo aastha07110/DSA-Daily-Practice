6ba0fbd2fd439536d3680eb3b27ae3d9c5fb0a03
aastha07110 <sha.aastha07@gmail.com>
2026-04-15 23:08:04 +0530

    Update 16-Power-Of-2.java

--- 01-Number-System/16-Power-Of-2.java
+++ 01-Number-System/16-Power-Of-2.java
@@ -8,15 +8,22 @@ import java.util.*;
 
 public class PowerOf2{
   static boolean isPowerOfTwo(int n){
-    if(n<=0)return false;
+    if(n <= 0) return false;
 
-    while(n%2==0){
-      n=n/2;
+    while(n % 2 == 0){
+      n = n / 2;
     }
-    return n==1;
+
+    return n == 1;
   }
+
   public static void main(String[] args){
-    Scanner sc =  new Scanner(System.in);
-    System.out.println(n + (isPowerOfTwo(n)? "Yes":"No");
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    System.out.println(n + (isPowerOfTwo(n) ? " is Power of 2" : " is NOT Power of 2"));
+
+    sc.close();
   }
 }
